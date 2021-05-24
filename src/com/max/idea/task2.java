627424b582be6b1345a8edbd623f6cc77a084f1e
RSalamatov <salamatov.roman7030@gmail.com>
2021-05-24 21:18:14 +0400

    Создан класс task2

--- src/com/max/idea/task2.java
+++ src/com/max/idea/task2.java
@@ -1,6 +1,12 @@
 package com.max.idea;
 
 public class task2 {
-    double b  = 3.1416;
-    byte c = 127;
+    public static void main(String[] args) {
+         double b  = 3.1416;
+         byte c = 127;
+    System.out.println(b);
+    System.out.println(c);
     }
+}
+
+
