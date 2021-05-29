6b24782fbd6b5595d2a7a150067363f2548e71b8
RSalamatov <salamatov.roman7030@gmail.com>
2021-05-29 18:30:56 +0400

    Создан класс task3

--- src/com/max/idea/task3.java
+++ src/com/max/idea/task3.java
@@ -1,4 +1,14 @@
 package com.max.idea;
 
 public class task3 {
+      public static void main(String[] args) {
+            int[] nums = {250, 2, 3, 4, 300};
+
+            int last = nums[nums.length-1];
+            int first = nums[nums.length-5];
+            nums[0] = last;
+            nums[4] = first;
+            int sum = nums[nums.length-5] + nums[nums.length-3];
+            System.out.println(sum);
+      }
 }
