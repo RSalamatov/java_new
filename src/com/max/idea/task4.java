a30b4c30c50db05a392440e5773d5abacb6251aa
RSalamatov <salamatov.roman7030@gmail.com>
2021-05-31 22:48:31 +0400

    Создан класс task4

---
+++ src/com/max/idea/task4.java
@@ -0,0 +1,19 @@
+package com.max.idea;
+
+
+
+public class task4 {
+    public static void main(String[] args) {
+        double[] nums = {6, 5, 8};
+        double result = 19;
+        System.out.println("Среднее арифметическое " + result / nums.length);
+        double result2 = 6.333333333333333;
+        System.out.println(result2 / 2);
+        double r3 = 3.1666666666666665;
+        System.out.println("Округление в меньшую сторону " + Math.floor(r3));
+        if(r3 > 3) {
+            System.out.println("Программа выполнена корректно");
+
+}
+    }
+}
