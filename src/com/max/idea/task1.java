57b9aaeb140c1e1c70f80f403b2b9f617004e29b
RSalamatov <salamatov.roman7030@gmail.com>
2021-05-31 23:06:50 +0400

    Создан класс task1

---
+++ src/com/max/idea/task1.java
@@ -0,0 +1,9 @@
+package com.max.idea;
+
+public class task1 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("java");
+
+    }
+}
