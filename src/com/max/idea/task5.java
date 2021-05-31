b45a86a4188d1c8f45b1e6f1a6953a60bc734920
RSalamatov <salamatov.roman7030@gmail.com>
2021-05-31 23:03:50 +0400

    Создан класс task5

---
+++ src/com/max/idea/task5.java
@@ -0,0 +1,12 @@
+package com.max.idea;
+
+
+
+public class task5 {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 7;
+        int result = a * b;
+        System.out.println(result);
+    }
+}
