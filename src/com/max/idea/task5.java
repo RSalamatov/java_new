62a620eff4d896a114de44d915db67fdb851ce15
RSalamatov <salamatov.roman7030@gmail.com>
2021-06-05 15:28:08 +0400

    Создан класс task5

--- src/com/max/idea/task5.java
+++ src/com/max/idea/task5.java
@@ -1,12 +1,41 @@
 package com.max.idea;
 
 
+import java.util.Scanner;
 
 public class task5 {
-    public static void main(String[] args) {
-        int a = 4;
-        int b = 7;
-        int result = a * b;
-        System.out.println(result);
-    }
-}
+
+
+    public static void main(String[] args){
+
+
+            System.out.println("Введите число:");
+            Scanner sc = new Scanner(System.in);
+            int number = sc.nextInt();
+
+            System.out.println("Введите число:");
+            int number2 = sc.nextInt();
+
+            System.out.println("Введите операцию:");
+
+            char sim = sc.next().charAt(0);
+
+            if (sim == '-') {
+
+                System.out.println("Результат операции: " + (number - number2));
+
+            } else if (sim == '+') {
+                    System.out.println("Результат операции: " + (number+number2));
+
+            } else if (sim == '*') {
+                        System.out.println("Результат операции: " + number*number2);
+
+            } else if (sim == '/') {
+                            System.out.println("Результат операции: " + number/number2);
+
+
+
+                        }
+                    }
+                }
+
