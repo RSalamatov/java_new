d9ae4eaef80b5af9f059c1ad69ac01be68a76561
RSalamatov <salamatov.roman7030@gmail.com>
2021-06-05 16:22:48 +0400

    Создан класс task4

--- src/com/max/idea/task4.java
+++ src/com/max/idea/task4.java
@@ -1,19 +1,30 @@
 package com.max.idea;
 
 
+import java.util.Scanner;
 
 public class task4 {
     public static void main(String[] args) {
-        double[] nums = {6, 5, 8};
-        double result = 19;
-        System.out.println("Среднее арифметическое " + result / nums.length);
-        double result2 = 6.333333333333333;
-        System.out.println(result2 / 2);
-        double r3 = 3.1666666666666665;
-        System.out.println("Округление в меньшую сторону " + Math.floor(r3));
-        if(r3 > 3) {
+        System.out.println("Введите число X: ");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println("Введите число Y: ");
+        int y = sc.nextInt();
+        System.out.println("Введите число Z: ");
+        int z = sc.nextInt();
+
+
+        double sum = x+y+z;
+        double srar = sum/3;
+        System.out.println(srar);
+        System.out.println("Среднее арифметическое= " + srar);
+        srar = srar/2;
+        System.out.println("Округление в меньшую сторону= " + Math.floor(srar));
+        if(srar > 3) {
             System.out.println("Программа выполнена корректно");
 
-}
+} else {
+            System.out.println("Выполните программу еще раз");
+        }
     }
 }
