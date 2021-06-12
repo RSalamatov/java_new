1cf1f391d72b3c7f412ad2f555893d5dea255d9b
RSalamatov <salamatov.roman7030@gmail.com>
2021-06-12 13:56:30 +0400

    Выполнена Задача №8_ Циклы

--- src/com/max/idea/task8.java
+++ src/com/max/idea/task8.java
@@ -9,12 +9,17 @@ public class task8 {
         System.out.println("Введите число:");
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
+
         int sum = 0;
+
         for (int i = 1; i <= number; i++) {
             if (i % 2 != 0) {
                 sum = sum + i;
             }
         }
-        System.out.println(sum);
+        if (number < 0){
+            System.out.println("Это не положительное число");
+        } else
+            System.out.println(sum);
     }
 }
