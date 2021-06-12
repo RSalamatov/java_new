1c33a7e24ba943d8eecbd5a461d65df140ec2288
RSalamatov <salamatov.roman7030@gmail.com>
2021-06-12 13:50:13 +0400

    Выполнена Задача №7_ Циклы_2

---
+++ src/com/max/idea/task8.java
@@ -0,0 +1,22 @@
+package com.max.idea;
+import java.util.Scanner;
+
+
+
+public class task8 {
+    public static void main(String[] args) {
+
+        System.out.println("Введите число:");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            if (i % 2 != 0) {
+                sum = sum + i;
+            }
+
+
+        }
+        System.out.println(sum);
+    }
+}
